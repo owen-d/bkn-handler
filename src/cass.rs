3079cfbfd29fa1db3bdc518621ee22a4b8c890fb
owen-d <ow.diehl@gmail.com>
2017-09-06 16:29:40 -0400

    [wip] fetching bkns from cass

--- src/cass.rs
+++ src/cass.rs
@@ -3,19 +3,25 @@
 // fn to add a pass-by (google bot interaction)
 extern crate cdrs;
 extern crate r2d2;
+extern crate uuid;
 
-use cdrs::client::CDRS;
-use cdrs::authenticators::NoneAuthenticator;
-use cdrs::transport::TransportTcp;
-use cdrs::connection_manager::ConnectionManager;
-use cdrs::compression::Compression;
-use cdrs::query::QueryBuilder;
-use uuid::Uuid;
+use eddystone::EddystoneUID;
+use self::cdrs::authenticators::NoneAuthenticator;
+use self::cdrs::transport::TransportTcp;
+use self::cdrs::connection_manager::ConnectionManager;
+use self::cdrs::compression::Compression;
+use self::cdrs::query::QueryBuilder;
+use self::cdrs::types::value::Value;
+use self::cdrs::types::IntoRustByName;
 
-pub struct Conn(r2d2::Pool<ConnectionManager>);
+use self::uuid::Uuid;
+use std::error::Error;
+use std::convert::Into;
+
+pub struct Conn(r2d2::Pool<ConnectionManager<NoneAuthenticator, TransportTcp>>);
 
 impl Conn {
-    pub fn new(addr: &str, size: int8) -> Result<Self, &str> {
+    pub fn new(addr: &str, size: u32) -> Result<Self, &str> {
         let config = r2d2::Config::builder()
             .pool_size(size)
             .build();
@@ -28,21 +34,56 @@ impl Conn {
             .map(|pool| Conn(pool))
     }
 
-    pub fn fetch_bkn_msg(&self) -> Result<Beacon, &str> {
+    pub fn fetch_bkn_msg(&self, eddy: &EddystoneUID) -> Result<Beacon, &str> {
         let pool = self.0.clone();
-        let values: Vec<Value> = vec![];
-        let query = QueryBuilder::new("SELECT name, msg_url FROM bkn.beaons_by_id WHERE user_id = ? LIMIT 1;").values(values).finalize();
-        let mut conn = pool.get()?;
+        let values: Vec<Value> = vec![eddy.to_vec().into()];
+        let query = QueryBuilder::new("SELECT name, msg_url, user_id FROM bkn.beaons_by_id WHERE \
+                                       name = ? LIMIT 1;")
+            .values(values)
+            .finalize();
 
-        let res = conn.query(query, false, false)?;
-        let res_body = res.get_body().unwrap();
+        let body = pool.get()
+            .map_err(|e| e.description())
+            .and_then(|conn| {
+                conn.query(query, false, false)
+                    .map_err(|e| e.description())
+            })
+            .and_then(|res| {
+                res.get_body()
+                    .map_err(|e| e.description())
+            })?;
 
-        if let Some(rows) = res_body.into_rows() {
-            let bkns: Vec<Beacon> = rows.iter()
-                .map(|row| {
-                    let mut bkn = Beacon{..Default::default()};
-                })
-        }
+        let none_matched = "no beacons matched";
+
+        body.into_rows()
+            .ok_or(none_matched)
+            .and_then(|rows| {
+                let bkns: Vec<Beacon> = rows.iter()
+                    .map(|row| {
+                        let mut bkn = Beacon { ..Default::default() };
+                        if let Ok(name) = row.get_r_by_name("name") {
+                            bkn.name = name;
+                        }
+
+
+                        if let Ok(msg_url) = row.get_r_by_name("msg_url") {
+                            bkn.msg_url = msg_url;
+                        }
+
+                        if let Ok(user_id) = row.get_r_by_name("user_id") {
+                            let _: Uuid = user_id;
+                            // bkn.user_id = user_id;
+                        }
+
+                        bkn
+                    })
+                    .take(1)
+                    .collect();
+                match bkns.len() {
+                    0 => Err(none_matched),
+                    _ => Ok(bkns.remove(0)),
+                }
+            })
     }
     // pub fn add_passby(){}
     // pub fn add_interaction(){}
@@ -50,7 +91,7 @@ impl Conn {
 
 #[derive(Debug, Default)]
 pub struct Beacon {
-    pub user_id: uuid,
+    pub user_id: Uuid,
     pub name: Vec<u8>,
     pub msg_url: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@ extern crate rocket;
 extern crate rocket_contrib;
 mod eddystone;
 mod referrer;
+mod cass;
 
 use rocket_contrib::Template;
 use eddystone::EddystoneUID;
 use referrer::Referrer;
 use rocket::response::Redirect;
+use rocket::State;
 
 fn main() {
+    let conn = cass::Conn::new("127.0.0.1", 15).unwrap();
     rocket::ignite()
         .mount("/", routes![handle_impression, handle_passby])
         .attach(Template::fairing())
+        .manage(conn)
         .launch();
 }
 
@@ -34,12 +38,12 @@ Need to do a few things on request lifecycle
  */
 
 #[get("/bkn/<_name>", rank=2)]
-fn handle_impression(_name: EddystoneUID, _referrer: Referrer) -> Redirect {
+fn handle_impression(_name: EddystoneUID, _referrer: Referrer, _conn: State<cass::Conn>) -> Redirect {
     Redirect::found("https://www.google.com")
 }
 
 #[get("/bkn/<_name>", rank=3)]
-fn handle_passby(_name: EddystoneUID) -> Template {
+fn handle_passby(_name: EddystoneUID, _conn: State<cass::Conn>) -> Template {
     let context = TemplateContext {
         name: format!("placeholder"),
     };
